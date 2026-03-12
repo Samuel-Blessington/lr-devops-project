0f911d30362a9744718253a5aab415cfa7e1c4e6
Samuel-Blessington <edasamuel2005@gmail.com>
2026-03-12 15:36:11 +0530

    testing pipeline

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
-X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
-y = np.array([3, 5, 7, 9, 11])
-model = LinearRegression()
-model.fit(X, y)
-prediction = model.predict([[6]])
-print(f"Predicted salary for 6 years experience: {prediction[0]}")
+print("DevOps Pipeline Debug Success")
